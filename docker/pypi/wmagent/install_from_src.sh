71411579d9f1c6f31299f405802ffa2deac94bc1
Dario Mapelli <mapelli.dario@gmail.com>
2025-03-28 13:18:22 +0100

    python3.12: wmagent added rudimentary install_from_src.sh script

--- docker/pypi/wmagent/install_from_src.sh
+++ docker/pypi/wmagent/install_from_src.sh
@@ -132,6 +132,11 @@ echo "Done $stepMsg!" && echo
 echo "-----------------------------------------------------------------------"
 
 tweakEnv(){
+    elif [ ! -f $ENV_FILE ]; then
+      echo -e "\n  Could not find $ENV_FILE, but I'm copying it now."
+      cp $PWD/WMCore/deploy/env.sh $ENV_FILE
+    fi
+
     # A function to apply environment tweaks for the docker image
     echo "Edit \$WMA_ENV_FILE script to point to \$WMA_ROOT_DIR"
     sed -i "s|/data/|\$WMA_ROOT_DIR/|g" $WMA_ENV_FILE
